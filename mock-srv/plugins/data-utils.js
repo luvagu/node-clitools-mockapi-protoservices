20dfa86ae206f14dd8500afe9ddd6a720b6a07ea
luvagu <luiavag@gmail.com>
2021-03-02 10:29:42 -0500

    extended fastify decorate fn

--- mock-srv/plugins/data-utils.js
+++ mock-srv/plugins/data-utils.js
@@ -44,10 +44,13 @@ const calculateID = (idPrefix, data) => {
 }
 
 module.exports = fp(async function (fastify, opts) {
+	fastify.decorate('currentOrders', currentOrders)
+	fastify.decorate('realtimeOrders', realtimeOrders)
 	fastify.decorateRequest('mockDataInsert', function insert(category, data) {
 		const request = this
 		const idPrefix = catToPrefix[category]
 		const id = calculateID(idPrefix, data)
+		orders[id] = { total: 0 }
 		data.push({ id, ...request.body })
 	})
 })
