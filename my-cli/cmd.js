104402f5fdc888fef7c8e08969d985851fc258f8
luvagu <luiavag@gmail.com>
2021-03-03 12:11:40 -0500

    added tui to interact with the cli and mimic the front end inputs

--- my-cli/cmd.js
+++ my-cli/cmd.js
@@ -3,6 +3,9 @@
 import got from 'got'
 import minimist from 'minimist'
 import commist from 'commist'
+import enquirer from 'enquirer'
+import chalk from 'chalk'
+import ansiEsc from 'ansi-escapes'
 
 const API = 'http://localhost:3000'
 
@@ -17,6 +20,12 @@ const usage = (msg = 'Back office for My App') => {
 	console.log('  cats:  my-cli list cats')
 	console.log('  ids:   my-cli list ids --cat=<string> --api=<string>')
 	console.log('  ids:   my-cli list ids -c=<string> --api=<string>')
+	console.log('\n-=interactive mode=-\n')
+	console.log('run: my-cli')
+	console.log('run: my-cli --api {API_URL}')
+	console.log('\n-=help=-\n')
+	console.log('run: my-cli --help')
+	console.log('run: my-cli -h\n')
 }
 
 const noMatches = commist()
@@ -24,9 +33,90 @@ const noMatches = commist()
 	.register('list cats', listCats)
 	.register('list ids', listIds)
 	.parse(process.argv.slice(2))
+
 if (noMatches) {
-	usage()
-	process.exit(1)
+	const args = minimist(process.argv.slice(2), {
+		boolean: ['help', 'man'],
+		alias: { help: 'h', man: 'm' },
+		string: ['api'],
+		default: { api: API },
+	})
+
+	const { api, help, man } = args
+
+	if (help || man) {
+		usage()
+		process.exit()
+	}
+
+	try {
+		await tui(api)
+	} catch (err) {
+		const cancelled = err === ''
+		if (cancelled === false) {
+			console.log(err.message)
+			process.exit(1)
+		}
+	}
+}
+
+async function tui(api) {
+	const { category } = await enquirer.prompt({
+		type: 'autocomplete',
+		name: 'category',
+		message: 'Category',
+		choices: categories,
+	})
+
+	let products = await got(`${api}/${category}`).json()
+	let quit = false
+
+	while (true) {
+		for (const { name, rrp, info } of products) {
+			console.log(chalk`{bold ${name}} - {italic ${rrp}}${info}`)
+		}
+
+		const form = new enquirer.Form({
+			message: 'Add',
+			hint: `Press Ctrl+X to change category`,
+			validate(values) {
+				const { name, rrp, info } = values
+				if (!name || !rrp || !info) return 'All fields are required'
+				if (Number.isFinite(Number(rrp)) === false)
+					return 'RRP should be a number'
+				return true
+			},
+			choices: [
+				{ name: 'name', message: 'Name' },
+				{ name: 'rrp', message: 'RRP' },
+				{ name: 'info', message: 'Info' },
+			],
+		})
+
+		form.on('keypress', (_, { ctrl, name }) => {
+			if (ctrl && name === 'x') {
+				form.cancel()
+				quit = true
+			}
+		})
+
+		let add = null
+
+		try {
+			add = await form.run()
+		} catch (err) {
+			if (quit) {
+				console.log(ansiEsc.clearTerminal)
+				return tui(api)
+			}
+			throw err
+		}
+
+		products = await got.post(`${api}/${category}`, { json: add }).json()
+
+		console.log(ansiEsc.clearTerminal)
+		console.log(chalk`{green ✔} {bold Category} {dim ·} {cyan ${category}}`)
+	}
 }
 
 async function addOrder(argv) {
